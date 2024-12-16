330c5770be3eb4525c8e27ec1cca8af14c08c26e
Mythili <mythilik908@gmail.com>
2024-12-15 19:27:33 -0600

    Remove API key file from version control

--- proxy.php
+++ proxy.php
@@ -1,28 +1,28 @@
 <?php
-  // put your Yelp API key here:
-  $API_KEY = 'mjUK0bjXgC1YYV7fnodOdqQu2UvWumyhT0--qNOJfGxWYoFPcSt5QTkvw0nKEKH_ogZOCThcHnO3gfXLX3FMVbYB3MsAo9CEk9ide1x0rG7aIlbjnjAS6Vd8bQ7uZnYx';
+require_once 'api_key.php';
+// put your Yelp API key here:
+$API_KEY = API_KEY;
 
-  $API_HOST = "https://api.yelp.com";
-  $SEARCH_PATH = "/v3/businesses/search";
-  $BUSINESS_PATH = "/v3/businesses/";
-  $curl = curl_init();
-  if (FALSE === $curl)
-     throw new Exception('Failed to initialize');
-  $url = $API_HOST . $SEARCH_PATH . "?" . $_SERVER['QUERY_STRING'];
-  curl_setopt_array($curl, array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,  // Capture response.
-            CURLOPT_ENCODING => "",  // Accept gzip/deflate/whatever.
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                "authorization: Bearer " . $GLOBALS['API_KEY'],
-                "cache-control: no-cache",
-            ),
-        ));
-  $response = curl_exec($curl);
-  curl_close($curl);
-  print $response;
-?>
+$API_HOST = "https://api.yelp.com";
+$SEARCH_PATH = "/v3/businesses/search";
+$BUSINESS_PATH = "/v3/businesses/";
+$curl = curl_init();
+if (FALSE === $curl)
+  throw new Exception('Failed to initialize');
+$url = $API_HOST . $SEARCH_PATH . "?" . $_SERVER['QUERY_STRING'];
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $url,
+  CURLOPT_RETURNTRANSFER => true,  // Capture response.
+  CURLOPT_ENCODING => "",  // Accept gzip/deflate/whatever.
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "authorization: Bearer " . $GLOBALS['API_KEY'],
+    "cache-control: no-cache",
+  ),
+));
+$response = curl_exec($curl);
+curl_close($curl);
+print $response;
